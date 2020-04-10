2c2f2ae82c1c19934e4bb012f33590493f650ef6
charles kwakye <ckmann1.kc@gmail.com>
2020-04-10 04:29:03 +0000

    server started

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 var express = require("express");
-var hbars= require("express3-handlbars");
+var hbars= require("express3-handlebars");
 var fs = require("fs");
-var bodyP= require("bodyparser");
+var bodyP= require("body-parser");
 var http = require("http");
+
+
 
 
 
 var xxp = express();
 xxp.set("port",process.env.PORT || 3000);
+xxp.get("/",(req,res)=>{
+    
+    res.send("hello world");
+})
+
 
 xxp.listen(xxp.get("port"),()=>{
     console.log("sever started on port " + xxp.get("port"));
